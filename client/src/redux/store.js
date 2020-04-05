7d280b0619ac75126a1d47c0cfbd77bcfbe1e594
aserantes-navent <aserantes@navent.com>
2020-04-05 03:54:13 -0300

    (createAction & createReducer) >>> createSlice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,19 +1,23 @@
-import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const toggleDarkMode = createAction('TOGGLE_DARK_MODE');
-
-const uiDefaultState = {
-  prefersDarkMode: false,
+const initialState = {
+  ui: {
+    prefersDarkMode: false,
+  },
 };
 
-const uiReducer = createReducer(uiDefaultState, {
-  [toggleDarkMode]: (state) => {
-    state.prefersDarkMode = !state.prefersDarkMode;
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState: initialState.ui,
+  reducers: {
+    toggleDarkMode: (state) => {
+      state.prefersDarkMode = !state.prefersDarkMode;
+    },
   },
 });
 
-const store = configureStore({
-  reducer: { ui: uiReducer },
-});
+export const { toggleDarkMode } = uiSlice.actions;
 
-export { toggleDarkMode, store };
+export const store = configureStore({
+  reducer: { ui: uiSlice.reducer },
+});
