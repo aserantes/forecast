0ddac59d79310deac826c9661ce73fb4a179e5b5
aserantes-navent <aserantes@navent.com>
2020-04-12 22:34:51 -0300

    prevSearches and some Forecast info formatting

--- client/src/components/Forecast.js
+++ client/src/components/Forecast.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Card, CardHeader, CardContent, CardMedia, Avatar, makeStyles } from '@material-ui/core';
 
-function Forecast(props) {
-  const { forecast } = props;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345
+  },
+  avatar: {
+    backgroundColor: 'rgba(0, 0, 0, 0.25)'
+  }
+}));
 
-  return <div>{forecast.coord.lon}</div>;
+function Forecast({ forecast }) {
+  const classes = useStyles();
+  const { main, name: cityName, weather } = forecast;
+  const { main: weatherMain, description: weatherDesc, icon: weatherIcon } = weather[0];
+  const { temp, pressure, humidity, temp_min: minTemp, temp_max: maxTemp } = main;
+  const today = new Date(Date.now()).toLocaleString().split(',')[0];
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar
+            alt={weatherDesc}
+            src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
+            className={classes.avatar}
+          />
+        }
+        title={`Forecast for ${cityName} - ${today}`}
+        subheader={`${weatherMain} (${weatherDesc})`}
+      />
+      <CardMedia />
+      <CardContent />
+    </Card>
+  );
 }
 
 export default Forecast;
--- client/src/components/PrevSearches.js
+++ client/src/components/PrevSearches.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import DarkModeToggler from './DarkModeToggler';
 
 function PrevSearches() {
   const previousCities = useSelector((state) => state.ui.previousCities);
   const renderPrevCities = () => {
-    return previousCities.map((prevCity) => <div key={prevCity.id}>{prevCity.name}</div>);
+    return (
+      <div>
+        <DarkModeToggler />
+        {previousCities.map((prevCity) => (
+          <div key={prevCity.id}>{prevCity.name}</div>
+        ))}
+      </div>
+    );
   };
   return renderPrevCities();
 }
