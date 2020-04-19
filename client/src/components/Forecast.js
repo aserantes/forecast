c56f67307332ab3f1f247fe5872e2a9d0e25ded3
aserantes-navent <aserantes@navent.com>
2020-04-18 21:14:28 -0300

    added google static map

--- client/src/components/Forecast.js
+++ client/src/components/Forecast.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, CardMedia, Avatar, makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345
-  },
+const useStyles = makeStyles(() => ({
   avatar: {
     backgroundColor: 'rgba(0, 0, 0, 0.25)'
   }
@@ -12,9 +9,15 @@ const useStyles = makeStyles((theme) => ({
 
 function Forecast({ forecast }) {
   const classes = useStyles();
-  const { main, name: cityName, weather } = forecast;
+  const { main, name: cityName, weather, coord } = forecast;
   const { main: weatherMain, description: weatherDesc, icon: weatherIcon } = weather[0];
   const { temp, pressure, humidity, temp_min: minTemp, temp_max: maxTemp } = main;
+  const { lon, lat } = coord;
+
+  const googleMapStaticUrl = 'https://maps.googleapis.com/maps/api/staticmap';
+  const publicApiKey = 'AIzaSyATSrlXeexQILWJpBpOehRMdeVeRowLq70';
+  const mapUrl = `${googleMapStaticUrl}?center=${lat},${lon}&zoom=10&scale=1&size=400x200&maptype=hybrid&key=${publicApiKey}`;
+
   const today = new Date(Date.now()).toLocaleString().split(',')[0];
   return (
     <Card>
@@ -29,8 +32,7 @@ function Forecast({ forecast }) {
         title={`Forecast for ${cityName} - ${today}`}
         subheader={`${weatherMain} (${weatherDesc})`}
       />
-      <CardMedia />
-      <CardContent />
+      <CardMedia component='img' src={mapUrl} />
     </Card>
   );
 }
